0c9abefeb6617999b7aa0bc21d4a5cebeb2974d8
seungwooj <seungwoo.aaron@gmail.com>
2019-09-22 01:43:01 +0900

    [Python] add class Connector to dump json

--- app/models/show_db.py
+++ app/models/show_db.py
@@ -4,20 +4,35 @@
 import MySQLdb
 # python path에 넣었으나 찾지 못해서 path 따로 지정
 import app.models.config as config
+import json
 
-db = MySQLdb.connect(host = config.HOST,
-                     user = config.USER,
-                     passwd = config.PASS,
-                     db = config.MYDB)
-cur = db.cursor()
+class Connector:
+    def __init__(self):
+        self.db = MySQLdb.connect(host = config.HOST,
+                             user = config.USER,
+                             passwd = config.PASS,
+                             db = config.MYDB)
+        self.cur = self.db.cursor()
 
-# to apply SQL
-cur.execute('SELECT * FROM coupang_sales_data')
+    def cur_execute(self):
+        # to apply SQL
+        self.cur.execute('SELECT * FROM coupang_sales_data')
+        result = []
+        for row in self.cur.fetchall():
+            id = row[0]
+            datetime = str(row[1])
+            product_name = row[2]
+            brand_name = row[3]
+            price = row[4]
+            amount = row[5]
+            json_string = f"{{'id': {id}, 'datetime' : {datetime}, 'product_name' : {product_name}, 'brand_name' : {brand_name}," \
+                          f"'price' : {price}, 'amount' : {amount}}}"
 
-for row in cur.fetchall():
-    print(row)
+            result.append(json_string)
+        result = json.dumps(result)
+        self.db.close()
+        return result
 
-db.close()
 
 
 
