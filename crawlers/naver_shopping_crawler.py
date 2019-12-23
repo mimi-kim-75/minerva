01b4702ad649736f44f95fda3dce6892fdff9af5
seungwooj <seungwoo.aaron@gmail.com>
2019-12-23 23:17:56 +0900

    [Python] modified crawler

--- crawlers/naver_shopping_crawler.py
+++ crawlers/naver_shopping_crawler.py
@@ -1,35 +1,70 @@
 from insert_db import Connector
 # parse following data - navershopping
 # """write crawler here"""
+from selenium import webdriver
+import pandas as pd
 
-channel = "naver"
-brand = "silvert"
-product_name = "silvert_fryingpan"
-real_price = 20000
-product_size = 20
-product_material = "metal"
-product_shape = "round"
-product_pattern = "flower"
-pick = 10000
-purchase = 8000  # naver only
-review = 4000  # naver only
-seller_number = 50  # naver only
-sales_date = "2018.04"  # naver only
+driver = webdriver.Chrome()
+driver.get('https://www.premierleague.com/tables')
 
+try:
+    driver.find_element_by_css_selector('.closeBtn').click()
+except:
+    print("no layer")
+
+eplranking_selector = driver.find_element_by_css_selector('#mainContent')
+items = eplranking_selector.find_elements_by_css_selector('.tableBodyContainer tr:not(.expandable)')
+
+df = pd.DataFrame(columns=["Position", "club", "played", "won", "drawn", "lost", "GF", "GA", "GD", "points"])
+
+for i in range(20):
+    Position = items[i].find_elements_by_css_selector('tr>#Tooltip')[0].text
+    club = items[i].find_elements_by_css_selector('td>a')[0].text
+    played = items[i].find_elements_by_css_selector('td')[3].text
+    won = items[i].find_elements_by_css_selector('td')[4].text
+    drawn = items[i].find_elements_by_css_selector('td')[5].text
+    lost = items[i].find_elements_by_css_selector('td')[6].text
+    GF = items[i].find_elements_by_css_selector('td')[7].text
+    GA = items[i].find_elements_by_css_selector('td')[8].text
+    GD = items[i].find_elements_by_css_selector('td')[9].text
+    points = items[i].find_elements_by_css_selector('td')[10].text
+
+    data = {"Position": Position,
+            "club": club,
+            "played": played,
+            "won": won,
+            "drawn": drawn,
+            "lost": lost,
+            "GF": GF,
+            "GA": GA,
+            "GD": GD,
+            "points": points}
+
+    # df.loc[len(df)] = data
+
+    driver.close
+#
+Posigtion = Position
+club = club
+played = played
+won = won
+drawn = drawn
+lost = lost
+GF = GF
+GA = GA
+GD = GD
+points = points
 # insert DB
 connector = Connector()
 connector.cur_insert(
-    channel,
-    brand,
-    product_name,
-    real_price,
-    product_size,
-    product_material,
-    product_shape,
-    product_pattern,
-    pick,
-    purchase,
-    review,
-    seller_number,
-    sales_date
+    Position,
+    club,
+    played,
+    won,
+    drawn,
+    lost,
+    GF,
+    GA,
+    GD,
+    points,
 )
